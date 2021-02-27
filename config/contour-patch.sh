441a8804c4a28b7a6f6ebecaaf5ce8d54c485e7b
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2021-02-27 09:16:43 +0100

    Remove old demo
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- config/contour-patch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl patch configmap/config-network \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"ingress.class":"contour.ingress.networking.knative.dev"}}'
