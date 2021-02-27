d9b4a23e206affbdd5e361238a04ec8890195607
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2021-02-27 23:47:00 +0100

    Add Kafka persistente storage
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- kafka/kafka_setup.sh
+++ kafka/kafka_setup.sh
@@ -9,6 +9,6 @@ kubectl apply -n kafka -f $(dirname $0)/strimzi-cluster-operator.yaml
 
 sleep 5
 
-kubectl -n kafka apply -f $(dirname $0)/kafka-ephemeral.yaml
+kubectl -n kafka apply -f $(dirname $0)/kafka.yaml
 kubectl apply -n kafka -f $(dirname $0)/user-tls.yaml
 kubectl apply -n kafka -f $(dirname $0)/user-sasl-scram-512.yaml
